2df8758975796a7b6be61b07274396100a1a6ae4
knight <195293329@qq.com>
2020-07-20 16:09:10 +0800

    图算法练习

---
+++ 堆.py
@@ -0,0 +1,21 @@
+import heapq
+lst = [7, 2, 5, 7, 3, 4]
+# ##################### 第一种方式 ###################################
+h = []  # 堆容器
+for i in lst:
+    heapq.heappush(h, i)  # 入堆
+print(heapq.heappop(h))   # 出堆
+print(heapq.nsmallest(2, h))  # 最大的前n个数（不推荐）
+print(heapq.nlargest(2, h))   # 最小的前n个数
+
+# ##################### 第二种方式 ###################################
+heapq.heapify(lst)
+print(heapq.heappop(lst))
+print(heapq.nsmallest(2, lst))
+print(heapq.nlargest(2, lst))
+# ######################### 小结 ######################################
+# heapify 将lst转化为堆，如果lst已经是正常堆序列，那么可以没有这一步
+# 假如lst不是堆序，又没有heapify那么结果是错的。
+# 转化为堆的方法：1、逐个heapq.heappush();2、直接heapify()
+# trick：默认小顶堆-->大顶堆 ## 反入-出反
+
--- 第2遍/图/743-网络延迟时间.py
+++ 第2遍/图/743-网络延迟时间.py
@@ -1,25 +1,33 @@
 from collections import defaultdict
+import heapq
 class Solution:
     def networkDelayTime(self, times, N: int, K: int) -> int:
         adjacent = defaultdict(list)
-        path = []
+        path = {}
         for vi, vj, time in times:
-            adjacent[vi].append((vj, time))
+            adjacent[vi].append((time, vj))
         cans = [(0, K)]
         count = 0
-        while count < N and cans:
-            count += 1
-            ti, vi = cans.pop()
-            path.append(ti)
-            for vj, time in adjacent[vi]:
-                cans.append((time + ti, vj))
-            cans.sort(reverse=True)
-        print(path)
-        if len(path) < N:
-            return -1
-        else:
-            return max(path)
 
-time = [[1,2,1],[2,3,7],[1,3,4],[2,1,2]]
-a = Solution()
-print(a.networkDelayTime(time, 3, 1))
+        while count < N and cans:
+            ti, vi = cans.pop()
+            if vi in path:  # 一定加这个，否则count的存在会导致没有求所有节点的最短路径
+                continue
+            path[vi] = ti
+            count += 1
+            for time, vj in adjacent[vi]:
+                if vj not in path:  # 这里也要注意
+                    cans.append((time + ti, vj))
+            cans.sort(reverse=True)
+
+        return max(path.values()) if len(path) == N else -1
+
+# times = [[3,5,78],[2,1,1],[1,3,0],[4,3,59],[5,3,85],[5,2,22],[2,4,23],[1,4,43],[4,5,75],[5,1,15],[1,5,91],[4,1,16],[3,2,98],[3,4,22],[5,4,31],[1,2,0],[2,5,4],[4,2,51],[3,1,36],[2,3,59]]
+# a = Solution()
+# print(a.networkDelayTime(times, 5, 5))
+
+# 堆优化：
+# ti, vi = heapq.heappop(cans)  # 出堆  VS cans.pop()
+# heapq.heappush(cans, (time + ti, vj))  # 入堆  VS cans.append((time + ti, vj))  and cans.sort()
+
+
