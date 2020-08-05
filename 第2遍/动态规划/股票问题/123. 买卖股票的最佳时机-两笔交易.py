a95ef4b1d6706bf68dc3a54b6c2237e1d87762bb
knight <1952933329@qq.com>
2020-08-05 10:27:57 +0800

    DP算法练习

---
+++ 第2遍/动态规划/股票问题/123. 买卖股票的最佳时机-两笔交易.py
@@ -0,0 +1,28 @@
+"""
+给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+注意: 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+输入: [3,3,5,0,0,3,1,4]
+输出: 6
+解释: 在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3 。
+     随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii
+
+当优化空间的时候，为了避免定义临时变量，一定要注意倒着动规
+"""
+class Solution:
+    def maxProfit(self, prices) -> int:
+        ll = len(prices)
+        if ll <2: return 0
+        if ll == 2: return max(0, prices[1]-prices[0])
+        one_in, res = 0, float("-inf")
+        one_out = two_in = two_out = float("-inf")
+
+        for i in range(1, ll):
+            increase = prices[i]-prices[i-1]
+            two_out = max(two_in + increase, two_out)
+            two_in  = max(two_in + increase, one_out)
+            one_out = max(one_in + increase, one_out)
+            one_in  = max(one_in + increase, 0)
+            res = max(res, one_out, two_out)
+        return max(0, res)
