e0ddc1770d50411b109397b129c14d04f6a9501e
knight <1952933329@qq.com>
2020-08-25 15:27:09 +0800

    单调栈算法练习

---
+++ 第2遍/单调栈/42-接雨水.py
@@ -0,0 +1,61 @@
+class Solution:
+    def trap(self, height) -> int:
+        height = height
+        st = []
+        res = 0
+        ll = len(height)
+        for i in range(ll):
+            if st and height[st[-1]] < height[i]:  # 遇到非单调情况
+                while st and height[st[-1]] < height[i]:
+                    index = st.pop()
+                    if st:
+                        w = i - st[-1] -1
+                        h = min(height[i], height[st[-1]]) - height[index]
+                        res += w*h
+            st.append(i)
+        return res
+    def trap2(self, height) -> int:
+        res = 0
+        ll = len(height)
+        left = [height[0] for _ in range(ll)]
+        right = [height[-1] for _ in range(ll)]
+        for i in range(1, ll):
+            left[i] = max(left[i-1], height[i])
+            right[ll-1-i] = max(right[ll-i], height[ll-1-i])
+        for i in range(ll):
+            res += min(left[i], right[i]) - height[i]
+        return res
+
+    def trap3(self, height) -> int:
+        ll = len(height)
+        left, right = 0, ll-1
+        left_max, right_max = float("-inf"), float("-inf")
+        res = 0
+        while left <= right:
+            left_max = max(left_max, height[left])
+            right_max = max(right_max, height[right])
+            if left_max <= right_max:  # 左侧节点的左边最大高度是确定的，右边多大没关系，只要比left-max大就行
+                res += left_max - height[left]
+                left += 1
+            else:  # 右侧节点的right-max是确定的，左边最大是多少没关系，只要比右边大就行
+                res += right_max - height[right]
+                right -= 1
+        return res
+
+
+
+
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+a = Solution()
+print(a.trap2(height))
+
+"""
+分析：维护一个单调递减栈，遇到比自己高的说明右边界可以确定，然后向左定出边界
+      关键是高度界定，雨水的累计也是按照层次加的，而不是纵向。
+      
+方法2：动规的求处每个位置左边最大值、右边最大值，然后累加
+方法3：双指针解法：两个指针分别从两侧向中间移动，较小的一端可以确定存水。
+       在2中我们先确定每个位置的两侧边界，然后逐个求出可能存储的水量
+       实际上可以利用单侧的值计算，假如右边界的值比左边界要大，那么就可以确定左指针能存多少水了
+       反之，可以计算右指针的值。
+"""
--- 第2遍/单调栈/84. 柱状图中最大的矩形.py
+++ 第2遍/单调栈/84. 柱状图中最大的矩形.py
@@ -1,22 +1,23 @@
 class Solution:
     def largestRectangleArea(self, heights) -> int:
-        st = []
-        res = 0
+        st = []  # 单调栈
+        res = 0  #　最大值
         ll = len(heights)
         for i in range(ll):
-            if st and heights[st[-1]] > heights[i]:
-                while st and heights[st[-1]] > heights[i]:
+            if st and heights[st[-1]] > heights[i]:  # 遇到比自己小的
+                while st and heights[st[-1]] > heights[i]:  # 直到弹成单调栈
                     index = st.pop()
                     h = heights[index]
-                    w = i - st[-1]-1 if st else i
+                    w = i - st[-1]-1 if st else i  # 索引在st[-1]到i之间都是长度比heights[index]要高的
+                                                   # 如果栈为空的话说明整个左边都比当前index要高。
                     res = max(res, h*w)
             st.append(i)
 
-        for i in range(len(st)):
+        for i in range(len(st)):  # 假如最后栈没有空的话
             if i == 0:
-                w = st[-1]+1
+                w = ll  # 整个数组最低值
             else:
-                w = st[-1] - st[i-1]
+                w = (ll-1) - st[i-1]  # 右边界-左边界
             res = max(res, heights[st[i]]*w)  # 高度*宽度
         return res
 
@@ -24,13 +25,12 @@ class Solution:
         heights = [0] + heights + [0]
         st = [0]
         res = 0
-        ll = len(heights)
-        for i in range(1, ll):
-            if st and heights[st[-1]] > heights[i]:
-                while st and heights[st[-1]] > heights[i]:
+        for i in range(len(heights)):
+            if st and heights[i] < heights[st[-1]]:
+                while st and heights[i] < heights[st[-1]]:
                     index = st.pop()
                     h = heights[index]
-                    w = i - st[-1]-1  # 保证栈非空
+                    w = i - st[-1] - 1
                     res = max(res, h*w)
             st.append(i)
         return res
@@ -41,8 +41,13 @@ print(a.largestRectangleArea(height))
 
 
 """
-方法1要考虑两种边界：空栈、遍历完成后栈非空(单调升)
-使用方法2，可以免去边界判断，很帅的方法
+题目来源：https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
 
+方法1要考虑两种边界：空栈、遍历完成后栈非空(单调升)
+使用方法2：添加哨兵 免去边界判断，很帅的方法
+
+方法1
 宽度计算：以 4 2 0 3 2 5为例，不要用固定值计算延伸宽度，而是动态的值(i-1,而不是0这种)
+特点：一遍遍历后，单调栈的第一个元素是整个heights最低的值，无论最后一个元素多大，都是
+      栈的最后一个元素。也就是说遍历完成后栈保存的是介于最低值和右边界值之间的数据
 """
